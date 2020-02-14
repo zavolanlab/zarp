c6341db2a169d0c70d89dff898be83f7f0fb4ffd
Alex Kanitz <alexander.kanitz@unibas.ch>
2020-02-14 16:10:38 +0100

    verify test workflow output against ground truth
    add tests to workflow integration test `tests/test_workflow_integration` that
    - verify that STAR alignments match expected alignments (based on "ground truth" files)
    - verify that Salmon gene quantification assign the correct number of reads to each gene (based on "ground truth files)
    
    resolves #49

--- tests/test_integration_workflow/test.local.sh
+++ tests/test_integration_workflow/test.local.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Tear down test environment
-trap 'rm -rf logs/ results/ .snakemake/ && cd $user_dir' EXIT  # quoted command is exected after script exits, regardless of exit status
+#trap 'rm -rf logs/ results/ .snakemake/ && cd $user_dir' EXIT  # quoted command is exected after script exits, regardless of exit status
+trap 'cd $user_dir' EXIT  # quoted command is exected after script exits, regardless of exit status
 
 # Set up test environment
 set -eo pipefail  # ensures that script exits at first command that exits with non-zero status
@@ -21,6 +22,8 @@ snakemake \
     --rerun-incomplete \
     --use-singularity \
     --singularity-args="--bind ${PWD}"
+
+# Check md5 sum of some output files
 find results/ -type f -name \*\.gz -exec gunzip '{}' \;
 find results/ -type f -name \*\.zip -exec sh -c 'unzip -o {} -d $(dirname {})' \;
 md5sum --check "expected_output.md5"
@@ -35,4 +38,46 @@ md5sum --check "expected_output.md5"
 #     -name \*\.zip \
 #     -exec sh -c 'unzip -o {} -d $(dirname {})' \;
 # md5sum $(cat expected_output.files) > expected_output.md5
+
+# Check whether STAR produces expected alignments
+# STAR alignments need to be fully within ground truth alignments for tests to pass; not checking 
+# vice versa because processing might cut off parts of reads (if testing STAR directly, add '-f 1' 
+# as additional option)
+echo "Verifying STAR output"
+result=$(bedtools intersect -F 1 -v -bed \
+    -a input_files/synthetic.mate_1.bed \
+    -b results/single_end/synthetic_10_reads_mate_1/map_genome/synthetic_10_reads_mate_1_Aligned.sortedByCoord.out.bam \
+    | wc -l)
+if [ $result != "0" ]; then
+    echo "Alignments for mate 1 reads are not consistent with ground truth"
+    exit 1
+fi
+result=$(bedtools intersect -F 1 -v -bed \
+    -a input_files/synthetic.mate_2.bed \
+    -b results/single_end/synthetic_10_reads_mate_2/map_genome/synthetic_10_reads_mate_2_Aligned.sortedByCoord.out.bam \
+    | wc -l)
+if [ $result != "0" ]; then
+    echo "Alignments for mate 1 reads are not consistent with ground truth"
+    exit 1
+fi
+result=$(bedtools intersect -F 1 -v -bed \
+    -a <(cat input_files/synthetic.mate_1.bed input_files/synthetic.mate_2.bed) \
+    -b results/paired_end/synthetic_10_reads_paired/map_genome/synthetic_10_reads_paired_Aligned.sortedByCoord.out.bam \
+    | wc -l)
+if [ $result != "0" ]; then
+    echo "Alignments for mate 1 reads are not consistent with ground truth"
+    exit 1
+fi
+
+# Check whether Salmon assigns reads to expected genes
+echo "Verifying Salmon output"
+diff \
+    <(cat results/single_end/synthetic_10_reads_mate_1/salmon_quant/quant.genes.sf | cut -f1,5 | tail -n +2 | sort -k1,1) \
+    <(cat input_files/synthetic.mate_1.bed | cut -f7 | sort | uniq -c | sort -k2nr | awk '{printf($2"\t"$1"\n")}')
+diff \
+    <(cat results/single_end/synthetic_10_reads_mate_2/salmon_quant/quant.genes.sf | cut -f1,5 | tail -n +2 | sort -k1,1) \
+    <(cat input_files/synthetic.mate_2.bed | cut -f7 | sort | uniq -c | sort -k2nr | awk '{printf($2"\t"$1"\n")}')
+diff \
+    <(cat results/paired_end/synthetic_10_reads_paired/salmon_quant/quant.genes.sf | cut -f1,5 | tail -n +2 | sort -k1,1) \
+    <(cat input_files/synthetic.mate_1.bed | cut -f7 | sort | uniq -c | sort -k2nr | awk '{printf($2"\t"$1"\n")}')
 
