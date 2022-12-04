efa03753975c561dfd0e52fa747b1eb6cc72e1fc
Maria Katsantoni <31883096+mkatsanto@users.noreply.github.com>
2022-12-04 22:01:27 +0100

    feat: addition of snakemake compatible format and linting (#104)

--- tests/test_format/test_snakemake_lint.sh
+++ tests/test_format/test_snakemake_lint.sh
@@ -2,19 +2,10 @@
 
 # This script is currently exiting with non-zero status.
 # This is expected behaviour though, as several parameters can't be inferred from the test files.
-# Snakemake called with --keep-incomplete in order to keep the created samples table for inspection.
 
 # Tear down test environment
 cleanup () {
     rc=$?
-    rm -rf .cache/
-    rm -rf .config/
-    rm -rf .fontconfig/
-    rm -rf .java/
-    rm -rf .snakemake/
-    rm -rf logs/
-    rm -rf results/
-    rm -rf Log.out
     cd $user_dir
     echo "Exit status: $rc"
 }
@@ -30,12 +21,7 @@ cd $script_dir
 
 # Run tests
 snakemake \
-    --snakefile="../../workflow/rules/htsinfer.smk" \
-    --restart-times=0 \
+    --snakefile="../../workflow/Snakefile" \
     --profile="../../profiles/local-singularity" \
-    --config outdir="results" \
-             samples="../input_files/htsinfer_samples.tsv" \
-             samples_out="samples_htsinfer.tsv" \
-    --notemp \
-    --keep-incomplete
-
+    --configfile="../input_files/config.yaml" \
+    --lint
